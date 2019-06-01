4720156650bc2c6afae5bbc4362a7493b8384e0b
Dimo-Desktop <dimitarbez@gmail.com>
2019-06-01 17:47:14 +0200

    Uniforms in openGL

--- src/application.cpp
+++ src/application.cpp
@@ -118,8 +118,7 @@ int main()
 	/* Initialize the library */
 	if (!glfwInit())
 		return -1;
-
-
+	
 	/* Create a windowed mode window and its OpenGL context */
 	window = glfwCreateWindow(720, 720, "Hello World", NULL, NULL);
 	if (!window)
